ca436dd3aafdfa92a715043f7a72fbf641ba7231
Roy Daniel <roydanielr@gmail.com>
2022-01-19 23:08:12 -0500

    game board and animation

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Transitions/Using_CSS_transitions
+// var f = document.getElementById('cooper-above');
+// document.addEventListener('click', function(ev){
+//     f.style.transform = 'translateY('+(ev.clientY-25)+'px)';
+//     f.style.transform += 'translateX('+(ev.clientX-25)+'px)';
+// },false);
