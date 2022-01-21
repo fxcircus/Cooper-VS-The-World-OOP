578959bf211189dbaab08303d7b07fad0b2fcd8e
Roy Daniel <roydanielr@gmail.com>
2022-01-21 16:42:27 -0500

    basic attack and print methods

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@
 // Global Variables 
 //////////////////////////////
 let storyProgress = 0;
+const cooperAttacks = [{name:"LICK", points:7}, {name:"Fish Breath", points:20}]
+const truckAttacks = [{name:"Honk", points:25}, {name:"Dump garbage", points:30}]
+const sprinklerAttacks = [{name:"Splash", points:30}, {name:"Hiss", points:40}]
+const fireworksAttacks = [{name:"Big Boom", points:50}, {name:"Smoke", points:40}]
+
+
 //////////////////////////////
 // DOM
 //////////////////////////////
@@ -9,16 +15,34 @@ const newGameModalEl = document.querySelector('#new-game-modal')
 const mapModal = document.querySelector('#game-map-modal')
 const fightModalEl = document.querySelector('#fight-modal')
 const newGameBtnEl = document.querySelector('.new-game-button')
-const attackButtonEl = document.querySelector('#attack-button')
+const battleButtonEl = document.querySelector('#attack-button')
+const textAreaEl = document.querySelector('#text-area')
+
+const cooperFighterImg = document.querySelector('#cooper-back-image')
+const trcukFighterImg = document.querySelector('#garbage-truck')
+const sprinklerFighterImg = document.createElement('img')
+const fireworksFighterImg = document.createElement('img')
+
+const cooperAtkOneEl = document.querySelector('#cooper-attack-one')
+const cooperAtkTwoEl = document.querySelector('#cooper-attack-two')
+const cooperItemsEl = document.querySelector('#cooper-items')
 
 newGameBtnEl.addEventListener('click', (evt) => {
     newGameModalEl.removeChild(newGameBtnEl)
     mapModal.classList.add('show')
 })
 
-attackButtonEl.addEventListener('click', (evt) => {
+battleButtonEl.addEventListener('click', (evt) => {
     mapModal.classList.remove('show')
     fightModalEl.classList.add('show')
+})
+
+cooperAtkOneEl.addEventListener('click', (evt) => {
+    attack (cooper, currentEnemy, cooperAttacks[0])
+})
+
+cooperAtkTwoEl.addEventListener('click', (evt) => {
+    attack (cooper, currentEnemy, cooperAttacks[1])
 })
 
 //////////////////////////////
@@ -26,8 +50,22 @@ attackButtonEl.addEventListener('click', (evt) => {
 //////////////////////////////
 
 // Timeout Message
+const printMessage = (message) => {
+    setTimeout(() => {
+        textAreaEl.textContent = message
+    }, 0);
+}
 
-// Attack (attakcer, target)
+// Attack
+const attack = (source, target, attack) => {
+    if (Math.random() < source.accuracy) {
+        target.hp -= attack.points
+        printMessage(`${source.name} DOES ${attack.points} POINTS!`)
+    } else {
+        printMessage(`${source.name} MISSED!`)
+    }
+}
+
 
 // Item
 
@@ -46,20 +84,25 @@ attackButtonEl.addEventListener('click', (evt) => {
 //////////////////////////////
 
 // Fighter (Name, HP, Accuracy, DOM elemnts)
-// Mothods: Kill
+class Fighter {
+    constructor (name, hp, accuracy, domImg, attacks, isEnemy = false) {
+        this.name = name
+        this.hp = hp
+        this.accuracy = accuracy
+        this.domPicture = domImg
+        this.isEnemy = isEnemy
+    }
+    kill = () => {
+        this.domPicture.syle.animation = 'fade-out'
+    }
+}
 
-// Cooper
-// [{Lick:7}, {Fish Breath: 20}]
-
-// Grabage Truck
-// [{Honk:25}, {Dump garbage: 30}]
-
-// Sprinkler
-// [{Splash:30}, {Hiss: 40}]
-
-// Fireworks
-// [{Big Boom:50}, {Smoke: 40}]
-
+// Instantiate classes:
+const cooper = new Fighter ('COOPER', 100, 0.8, cooperFighterImg, cooperAttacks)
+const grabageTruck = new Fighter ('GARBAGE TRUCK', 40, 0.9, trcukFighterImg, truckAttacks, true)
+const sprinkler = new Fighter ('SPRINKLER', 80, 0.7, sprinklerFighterImg, sprinklerAttacks, true)
+const fireworks = new Fighter ('FIREWORKS', 120, 0.6, fireworksFighterImg, fireworksAttacks, true)
+let currentEnemy = grabageTruck
 //////////////////////////////
 // Fight Runtime
 //////////////////////////////
